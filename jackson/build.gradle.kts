e058ba25393c957da7c3854f5c585c7e67ebb552
Kjetil Valstadsve <kjetilv.github@vlitejk.cotse.net>
2023-12-03 15:01:09 +0100

    Split out jackson deps

--- jackson/build.gradle.kts
+++ jackson/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
 
     testImplementation("org.assertj:assertj-core:3.24.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
 }
 
